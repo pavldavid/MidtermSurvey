1df5d724c690bad82b8ecdbd96a45196c04971db
David Pavlenko <Dpavlenko@mail.greenriver.edu>
2021-05-16 23:30:09 -0400

    defined a default route that displays a header and link to the survery

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//Controller for the cupcakes site
+
+//Turn on error-reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+session_start();
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Define default route
+$f3->route('GET /', function(){
+    //Display the home page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run Fat-Free
+$f3->run();
